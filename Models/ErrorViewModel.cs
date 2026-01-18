94607e75a00203861cdee70e31031024013991a1
EmreSeyfula <emre.islam198@gmail.com>
2026-01-18 10:59:18 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlainSystemToPracticeForExamsMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
